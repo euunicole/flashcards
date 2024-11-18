9293df77532be13b0974e3a8c56eed04e884c606
euunicole <nicolegabriele2803@gmail.com>
2024-11-18 19:22:05 -0300

    Add files via upload

---
+++ Flashcards/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Raphael Montes',
+    'No livro Jantar Secreto, quem são os protagonistas da história?',
+    'Os protagonistas são Dante, Miguel, Hugo e Leitão.'
+)
+
+criaCartao(
+    'Colleen Hoover',
+    'No livro "Verity",qual era o propósito principal de Lowen ao aceitar o trabalho de finalizar os livros de Verity?',
+    'Lowen foi contratada porque a autora estava incapaz de continuar devido ao seu estado de saúde. '
+)
+
+criaCartao(
+    'Jenny Han',
+    ' No livro "O verão que mudou a minha vida", como Susannah Fisher influencia a história?',
+    'Susannah é uma mãe adotiva para Belly, e sua doença marca o fim de uma fase da vida de Belly e da família.'
+)
+
+criaCartao(
+    'Colleen Hoover',
+    ' No livro "É assim que acaba", o que faz com que Lily se apaixone por Ryle?',
+    'Lily se apaixona por Ryle devido à sua atração física, inteligência e confiança.'
+)
+
+criaCartao(
+    'Raphael Montes',
+    'Qual é o grande conflito de "Dias Perfeitos"?',
+    'O grande conflito do livro é o desequilíbrio mental de Teo, que se transforma em obsessão.'
+)
+
+criaCartao(
+    'Hannah Nicole',
+    'Quem é a protagonista de "Assistente do Vilão" e qual é sua função?',
+    'A protagonista do livro é Mina, uma jovem que trabalha como assistente de um vilão conhecido. Ela desempenha o papel de auxiliar em várias tarefas e atividades'
+)
